b52b6a95e66deb339993712779818100cc807888
pinghow0220 <leepinghow0220@gmail.com>
2026-08-31 23:47:25 +0800

    Push 3 small scripts

---
+++ password_validator.py
@@ -0,0 +1,5 @@
+password = input("Enter your password: ")
+if len(password) >= 8:
+    print("Password is valid.")
+else:
+    print("Password is too short.")
---
+++ user_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+country = input("Enter your country: ")
+
+print("Hello " + name + "!")
+print("You are " + str(age) + " years old.")
+print("You are from " + country + ".")
